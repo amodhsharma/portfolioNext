c0153387c584f1b4df20dc242a94be7cd4b1b512
Amodh Sharma <amodhextra@gmail.com>
2024-09-13 21:21:47 +0530

    second stable

--- components/ScrollIntroduction.js
+++ components/ScrollIntroduction.js
@@ -3,17 +3,20 @@ import styles from '../src/app/page.module.css';
 
 const ScrollIntroduction = () => {
   return (
-    <section id="introduction" 
+    <section id="introduction" className={styles.scrollIntroduction}
           style={{
+            fontFamily: '',
             display: 'flex', 
             flexDirection: 'column', 
             justifyContent: 'center',
             height: '100vh',
             margin: '0 5%',
+            fontSize: '1.8vw',
+            fontWeight: '400',
           }}>
-            <span style={{ fontSize: '1.5rem', fontWeight: 200, display: 'block'}}>Being brought up in a house full of fabrics, designs, colors and patterns does something to you,</span>
-            <span style={{ fontSize: '1.5rem', fontWeight: 200, display: 'block', marginTop: '1rem' }}> Something which makes you look at the whole world like its your canvas,</span>
-            <span style={{ fontSize: '1.5rem', fontWeight: 200, display: 'block', marginTop: '1rem' }}>What better sign from the universe to turn the world a bit more aesthetic, one click at a time </span>
+            <span style={{ display: 'block'}}>Being brought up in a house full of fabrics, designs, colors and patterns does something to you,</span>
+            <span style={{ display: 'block', marginTop: '1vw' }}> Something which makes you look at the whole world like its your canvas,</span>
+            <span style={{ display: 'block', marginTop: '1vw' }}>What better sign from the universe to turn the world a bit more aesthetic, one click at a time </span>
     </section>
   );
 }
--- components/StaticOne.js
+++ components/StaticOne.js
@@ -6,20 +6,33 @@ const StaticOne = () => {
         <div className={styles.static}>
           <div className={styles.staticMainText}
           style={{
-            fontSize: '12rem',
+            fontSize: '9vw',
             color: '#CAD2C5'
           }}>
-            <span style={{ fontWeight: 100, display: 'block' }}>Amodh</span>
-            <span style={{ fontWeight: 600, display: 'block', marginTop: '7rem', textAlign:'right' }}> शर्मा</span>
+            <span style={{ fontWeight: 100, display: 'block' }}>Amodh</span> {/* 100 is minimum*/}
+            <span style={{ fontWeight: 750, display: 'block', marginTop: '2vw', textAlign:'right' }}> शर्मा</span>
           </div>
-          <div className={styles.staticMainIntro}>
+          <div className={styles.staticMainIntro}
+          style={{
+            fontSize: '1.25vw',
+            color: '#CAD2C5',
+            margin: '5vw'
+          }}>
             <span style={{ fontWeight: 200, display: 'block' }}> A fresher with a knack for aesthetics. With a cobined experience of 0 years, I want to learn and expand in the field of visual and interactive web design</span>
             {/* display: Block - changes the default behavior of inline elements (like <span>) to that of block-level elements. */}
             {/* <p>A fresher with a knack for aesthetics. With a cobined experience of 0 years, I want to learn and expand in the field of visual and interactive web design</p> */}
             <span style={{ fontWeight: 200, display: 'block', marginTop: '1rem' }}> I value clarity, empathy, and integrity above all else. They guide my approach to problem solving and life in general.</span>
             {/* <p>I value clarity, empathy, and integrity above all else. They guide my approach to problem solving and life in general.</p> */}
           </div>
-          <div className={styles.staticMainLinks}>
+          <div className={styles.staticMainLinks}
+          style={{
+            fontSize: '1.25vw',
+            margin: '0 0 10vh 0',
+            display: 'flex',
+            flexDirection: 'column', // Arranges the items vertically
+            alignItems: 'flex-start', // Align items to the left (change to 'center' if you want centered alignment)
+            
+          }}>
             <span style={{ fontWeight: 200, display: 'block' }}><a href='#introduction'>00 Introduction</a></span>
             {/* <a href='#introduction'>00 Introduction</a>, href will be made to the section id of the desired location */}
             <span style={{ fontWeight: 200, display: 'block', marginTop: '0.5rem' }}><a href='#areas-of-interest'>01 Areas of Interest and Design Philosophy</a></span>
